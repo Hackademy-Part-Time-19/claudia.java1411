3ea5406e230dec24bb980125233fc6e886ce8cfd
claudia <claudia.matei@libero.it>
2023-11-19 11:30:56 +0100

    java1411

---
+++ java1411.js
@@ -0,0 +1,9 @@
+let quantita_stock = 30;
+
+let ordine = parseInt(prompt("Inserisci la quantita desiderata"))
+
+if (ordine - quantita_stock >= 0) {
+    let esitoOrdine = prompt("Ordine confermato ")
+} 
+else {
+    alert("Unita' non disponibili")}
